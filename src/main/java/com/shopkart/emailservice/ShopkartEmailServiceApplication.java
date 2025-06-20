68af99213a0c506d444e21238cbfe4d7335c1ac4
Sagar <patelsagar913@gmail.com>
2025-06-20 17:41:06 +0530

    first commit to init the project

---
+++ src/main/java/com/shopkart/emailservice/ShopkartEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopkart.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopkartEmailServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopkart/emailservice/ShopkartEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopkart.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartEmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
